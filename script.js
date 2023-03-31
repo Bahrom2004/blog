b229ba00fecf30efb90486f7d785bc505af8863e
Bahrom <79355757+Bahrom2004@users.noreply.github.com>
2023-03-31 13:15:29 +0500

    changing age

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["17 y.o.", "BI Developer", "Front End Developer"],
+        strings: ["18 y.o.", "BI Developer", "Front End Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["17 y.o.", "BI Developer", "Front End Developer"],
+        strings: ["18 y.o.", "BI Developer", "Front End Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
